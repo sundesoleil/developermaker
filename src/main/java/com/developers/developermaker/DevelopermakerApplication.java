bd2f692ec188d02ec8a91ad63870dae77b03b578
sundesoleil <dxrans@gmail.com>
2021-08-30 19:18:36 +0900

    Initial commit

---
+++ src/main/java/com/developers/developermaker/DevelopermakerApplication.java
@@ -0,0 +1,13 @@
+package com.developers.developermaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopermakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopermakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developers/developermaker/DevelopermakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developers.developermaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopermakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
