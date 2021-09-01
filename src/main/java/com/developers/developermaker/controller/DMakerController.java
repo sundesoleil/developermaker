5dc1e3b17e635fa96ec1d36a46f316cdf50652be
sundesoleil <dxrans@gmail.com>
2021-09-01 16:36:06 +0900

    db 저장 기능 구현

--- src/main/java/com/developers/developermaker/controller/DMakerController.java
+++ src/main/java/com/developers/developermaker/controller/DMakerController.java
@@ -1,16 +1,22 @@
 package com.developers.developermaker.controller;
 
+import com.developers.developermaker.service.DMakerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class DMakerController {
+
+    private final DMakerService dMakerService;
 
     @GetMapping("/developers")
     public List<String> getAllDevelopers (){
@@ -18,5 +24,14 @@ public class DMakerController {
         log.info("GET /developers HTTP/1.1");
 
         return Arrays.asList("snow", "Elsa", "Olaf");
+    }
+
+    @GetMapping("/create-developer")
+    public List<String> createDevelopers() {
+        log.info("GET /create-developer HTTP/1.1");
+
+        dMakerService.createDeveloper();
+
+        return Collections.singletonList("Olaf"); // 단일 객체 리스트
     }
 }
---
+++ src/main/java/com/developers/developermaker/service/DMakerService.java
@@ -0,0 +1,28 @@
+package com.developers.developermaker.service;
+
+import com.developers.developermaker.entity.Developer;
+import com.developers.developermaker.repository.DeveloperRepository;
+import com.developers.developermaker.type.DeveloperLevel;
+import com.developers.developermaker.type.DeveloperSkillType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DMakerService {
+    private final DeveloperRepository developerRepository;
+
+    @Transactional
+    public void createDeveloper() {
+        Developer developer = Developer.builder() // Developer Entity를 builder를 통해 각각의 데이터 세팅
+                .developerLevel(DeveloperLevel.JUNIOR)
+                .developerSkillType(DeveloperSkillType.BACK_END)
+                .experienceYears(2)
+                .name("Olaf")
+                .age(35)
+                .build();
+
+        developerRepository.save(developer); // 빌드 완료한 디벨로퍼를 레퍼지토리에 세이브함으로써 영속화(저장)
+    }
+}
