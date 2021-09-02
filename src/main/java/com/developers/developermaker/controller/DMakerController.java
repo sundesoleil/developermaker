7ba32781a3a67a22bfc9a41c2a2e1332a5229517
sundesoleil <dxrans@gmail.com>
2021-09-02 17:24:20 +0900

    data validation 적용

--- src/main/java/com/developers/developermaker/controller/DMakerController.java
+++ src/main/java/com/developers/developermaker/controller/DMakerController.java
@@ -5,6 +5,7 @@ import com.developers.developermaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class DMakerController {
         return Arrays.asList("snow", "Elsa", "Olaf");
     }
 
-    @GetMapping("/create-developer")
+    @PostMapping("/create-developer")
     public List<String> createDevelopers(
             @Valid @RequestBody CreateDeveloper.Request request
             ) {
