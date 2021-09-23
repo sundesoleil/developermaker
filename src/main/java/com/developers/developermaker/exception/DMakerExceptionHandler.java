109eb914a3fd69640aac213bdeb248b0a8ed36c5
sundesoleil <dxrans@gmail.com>
2021-09-23 18:13:49 +0900

    Http 요청 오류 및 validation 오류시 예외 처리

--- src/main/java/com/developers/developermaker/exception/DMakerExceptionHandler.java
+++ src/main/java/com/developers/developermaker/exception/DMakerExceptionHandler.java
@@ -2,10 +2,14 @@ package com.developers.developermaker.exception;
 
 import com.developers.developermaker.dto.DMakerErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.developers.developermaker.exception.DMakerErrorCode.INVALID_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
@@ -25,6 +29,24 @@ public class DMakerExceptionHandler {
         return DMakerErrorResponse.builder()
                 .errorCode(e.getDMakerErrorCode())
                 .errorMessage(e.getDetailMessage())
+                .build();
+    }
+
+    @ExceptionHandler(value = {
+            HttpRequestMethodNotSupportedException.class,
+            MethodArgumentNotValidException.class
+    })
+    public DMakerErrorResponse handlerBadRequest(
+            Exception e, HttpServletRequest request
+    ) {
+        log.error("url: {}, message: {}",
+                request.getRequestURI(),
+                e.getMessage()
+        );
+
+        return DMakerErrorResponse.builder()
+                .errorCode(INVALID_REQUEST)
+                .errorMessage(INVALID_REQUEST.getMessage())
                 .build();
     }
 }
