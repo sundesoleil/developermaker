89b260b9ea8a3691198516588d02f7b18c6a7efa
sundesoleil <dxrans@gmail.com>
2021-09-29 15:02:55 +0900

    Mockito를 활용한 개발자 생성 테스트 코드 작성

--- src/test/java/com/developers/developermaker/service/DMakerServiceTest.java
+++ src/test/java/com/developers/developermaker/service/DMakerServiceTest.java
@@ -1,37 +1,58 @@
 package com.developers.developermaker.service;
 
-import com.developers.developermaker.dto.CreateDeveloper;
-import com.developers.developermaker.dto.DeveloperDto;
-import com.developers.developermaker.type.DeveloperLevel;
+import com.developers.developermaker.code.StatusCode;
+import com.developers.developermaker.dto.DeveloperDetailDto;
+import com.developers.developermaker.entity.Developer;
+import com.developers.developermaker.repository.DeveloperRepository;
+import com.developers.developermaker.repository.RetiredDeveloperRepository;
+
 import com.developers.developermaker.type.DeveloperSkillType;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.developers.developermaker.type.DeveloperLevel.SENIOR;
+import static com.developers.developermaker.type.DeveloperSkillType.BACK_END;
+import static com.developers.developermaker.type.DeveloperSkillType.FRONT_END;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DMakerServiceTest {
 
-    @Autowired
+    @Mock
+    private DeveloperRepository developerRepository;
+
+    @Mock
+    private RetiredDeveloperRepository retiredDeveloperRepository;
+
+    @InjectMocks
     private DMakerService dMakerService;
 
     @Test
     public void createDeveloperTest() {
-        dMakerService.createDeveloper(CreateDeveloper.Request.builder()
-                .developerLevel(DeveloperLevel.SENIOR)
-                .developerSkillType(DeveloperSkillType.BACK_END)
-                .experienceYears(12)
-                .memberId("memberId")
-                .name("name")
-                .age(40)
-                .build()
-        );
-        List<DeveloperDto> employedDevelopers = dMakerService.getEmployedDevelopers();
-        System.out.println("=================");
-        System.out.println(employedDevelopers);
-        System.out.println("=================");
+
+        // Mock들의 동작 정의
+        given(developerRepository.findByMemberId(anyString()))
+                .willReturn(Optional.of(Developer.builder()
+                                .developerLevel(SENIOR)
+                                .developerSkillType(DeveloperSkillType.BACK_END)
+                                .experienceYears(12)
+                                .statusCode(StatusCode.EMPLOYED)
+                                .name("name")
+                                .age(45)
+                                .build()));
+
+        DeveloperDetailDto developerDetail = dMakerService.getDeveloperDetail("memberId");
+
+        assertEquals(SENIOR, developerDetail.getDeveloperLevel());
+        assertEquals(BACK_END, developerDetail.getDeveloperSkillType());
+        assertEquals(12, developerDetail.getExperienceYears());
     }
+
 }
