9d4f63037bb3e185275a9dd0aea083ccf20875c9
sundesoleil <dxrans@gmail.com>
2021-10-04 13:01:17 +0900

    서비스 테스트 코드 작성

--- src/test/java/com/developers/developermaker/service/DMakerServiceTest.java
+++ src/test/java/com/developers/developermaker/service/DMakerServiceTest.java
@@ -1,6 +1,7 @@
 package com.developers.developermaker.service;
 
 import com.developers.developermaker.code.StatusCode;
+import com.developers.developermaker.dto.CreateDeveloper;
 import com.developers.developermaker.dto.DeveloperDetailDto;
 import com.developers.developermaker.entity.Developer;
 import com.developers.developermaker.repository.DeveloperRepository;
@@ -9,6 +10,7 @@ import com.developers.developermaker.repository.RetiredDeveloperRepository;
 import com.developers.developermaker.type.DeveloperSkillType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -21,6 +23,8 @@ import static com.developers.developermaker.type.DeveloperSkillType.FRONT_END;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class DMakerServiceTest {
@@ -53,6 +57,34 @@ class DMakerServiceTest {
         assertEquals(SENIOR, developerDetail.getDeveloperLevel());
         assertEquals(BACK_END, developerDetail.getDeveloperSkillType());
         assertEquals(12, developerDetail.getExperienceYears());
+    }
+
+    @Test
+    void createDeveloperTest_success() {
+        //given
+        CreateDeveloper.Request request = CreateDeveloper.Request.builder()
+                .developerLevel(SENIOR)
+                .developerSkillType(DeveloperSkillType.BACK_END)
+                .experienceYears(12)
+                .memberId("memberId")
+                .name("name")
+                .age(45)
+                .build();
+        given(developerRepository.findByMemberId(anyString()))
+                .willReturn(Optional.empty());
+        ArgumentCaptor<Developer> captor =
+                ArgumentCaptor.forClass(Developer.class);
+
+        //when
+        dMakerService.createDeveloper(request);
+
+        //then
+        verify(developerRepository, times(1))
+                .save(captor.capture());
+        Developer savedDeveloper = captor.getValue();
+        assertEquals(SENIOR, savedDeveloper.getDeveloperLevel());
+        assertEquals(BACK_END, savedDeveloper.getDeveloperSkillType());
+        assertEquals(12, savedDeveloper.getExperienceYears());
     }
 
 }
